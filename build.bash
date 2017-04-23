4da62b28ccd3da3f2d1fa4bc2b4ae4c89270d18d
Benoît Zugmeyer <bzugmeyer@gmail.com>
2017-04-23 19:51:18 +0200

    expose an env-agnostic bundle by default

--- build.bash
+++ build.bash
@@ -3,12 +3,14 @@
 set -euo pipefail
 
 rm -rf build
-echo "Building for production..."
-rollup -c --external preact --format umd --output build/preact-motion.js --environment NODE_ENV:production
-echo "Building for dev..."
-rollup -c --external preact --format umd --output build/preact-motion.dev.js --environment NODE_ENV:development
-echo "Building for esnext dev..."
+echo "Building default bundle..."
+rollup -c --external preact --format umd --output build/preact-motion.js --environment NODE_ENV:
+echo "Building esnext bundle..."
 rollup -c --external preact --format es  --output build/preact-motion.es.js --environment NODE_ENV:
+echo "Building development bundle..."
+rollup -c --external preact --format umd --output build/preact-motion.dev.js --environment NODE_ENV:development
+echo "Building production bundle..."
+rollup -c --external preact --format umd --output build/preact-motion.prod.js --environment NODE_ENV:production
 
 echo "Generating flow types..."
 # Idea from https://github.com/facebook/flow/issues/1996#issuecomment-228925018
